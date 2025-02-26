8a7f245ef1999b4d2083e21f80dd757444ca4bc7
harleywelsby <harleywelsby9@gmail.com>
2025-02-26 17:38:05 +1300

    Add Vercel analytics component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './shared/styles/App.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Outlet />
       {/* TODO: Re-add when complete */}
       {/* <Footer /> */}
+      <Analytics />
     </>
   );
 }
