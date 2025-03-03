f191b0d6fbec30b30554c163193ffdc80bce497f
harleywelsby <harleywelsby9@gmail.com>
2025-03-03 16:51:05 +1300

    Try without wait-on for server

--- cypress/e2e/navbarTests.cy.js
+++ cypress/e2e/navbarTests.cy.js
@@ -1,7 +1,7 @@
 // TODO
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://localhost:4173');
+    cy.visit('http://localhost:4173');
     cy.get('[data-test-id="navbar-home-link"').should('be.visible');
   });
 });
