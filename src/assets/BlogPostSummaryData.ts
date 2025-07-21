45dd415abdcaa588410a489ec4e575ae386805ea
harleywelsby <harleywelsby9@gmail.com>
2025-07-21 18:38:37 +1200

    Latest batch of photos

--- src/assets/BlogPostSummaryData.ts
+++ src/assets/BlogPostSummaryData.ts
@@ -1,4 +1,14 @@
 export const BlogPostSummaryData = [
+  // TODO
+  // {
+  //   postId: '5d3f90ba-127a-427b-8779-e54883da97a7',
+  //   filePath: '/BlogPosts/BeginnersGuideToRomanImperialCoins.md',
+  //   title: 'A Guide to Roman Imperial Coins',
+  //   imagePath: '/Images/Blog/CaracallaGetaAssassination.webp',
+  //   imageAltText:
+  //     '"Geta Dying in his Mother\'s Arms" by Jacques-Augustine-Catherine Pajou, 1766-1828',
+  //   imageCredit: 'Wikimedia, José Luiz',
+  // },
   {
     postId: 'e3278fa9-5e60-4109-8643-18c36d559662',
     filePath: '/BlogPosts/GetaCaracallaAndProvidence.md',
